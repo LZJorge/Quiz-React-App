86b7cba4343aa393a4c99a9f567a07f106298b50
Jorge Landaeta <dev.jorge2003@hotmail.com>
2024-08-05 21:22:01 -0400

    fix: Verify token expiration

--- src/context/LoginContext.tsx
+++ src/context/LoginContext.tsx
@@ -1,4 +1,31 @@
 import { useState ,createContext, Dispatch, SetStateAction, ReactNode } from 'react'
+import { jwtDecode } from "jwt-decode";
+
+const isTokenExpired = (token: string) => {
+  if (!token) return true;
+  try {
+    const decodedToken = jwtDecode(token);
+    const currentTime = Date.now() / 1000;
+    return decodedToken.exp! < currentTime;
+  } catch (error) {
+    console.error("Error decoding token:", error);
+    return true;
+  }
+};
+
+const setToken = () => {
+  const token = localStorage.getItem("accessToken")
+
+  if(!token) {
+    return false
+  }
+
+  if(isTokenExpired(token)) {
+    return false
+  }
+
+  return true
+};
 
 interface LoginContextProps {
   auth: boolean
@@ -6,18 +33,18 @@ interface LoginContextProps {
 }
 
 export const LoginContext = createContext<LoginContextProps>({
-  auth: localStorage.getItem('accessToken') ? true : false,
-  setAuth: () => { return }
-})
+  auth: setToken(),
+  setAuth: () => {
+    return;
+  },
+});
 
 interface LoginProviderProps {
   children: ReactNode
 }
 
 export const LoginProvider: React.FC<LoginProviderProps> = ({ children }) => {
-  const [auth, setAuth] = useState(
-    localStorage.getItem('accessToken') ? true : false
-  )
+  const [auth, setAuth] = useState(setToken());
 
   return (
     <LoginContext.Provider value={{ auth, setAuth }}>
